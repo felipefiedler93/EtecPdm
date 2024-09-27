32b329800f701b94b4f1c4b28ef6a95a67c517f5
felipefiedler93 <fefiedler452@gmail.com>
2024-09-27 13:36:02 -0300

    Add files via upload

---
+++ A04_Exercício.kt
@@ -0,0 +1,20 @@
+fun main() {
+    // B:
+    var nome: String = "Felipe Fiedler de Oliveira"
+
+    // C:
+    var text: String? = null
+
+    // D:
+    val numeroValor: Byte = 40
+
+    // E:
+    val pibBrasileiro: Long = 10_900_000_000_000_000L
+
+    // F:
+    val cidadaoBr: Int = 212_600_000
+
+    // G:
+    val pibCapita = pibBrasileiro / cidadaoBr
+    println("PIB per capita: R$$pibCapita")
+}
